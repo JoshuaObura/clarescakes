d259d599ea918cd6638c65bf80ba3218b86e858f
jh <oluashegun@googlemail.com>
2020-10-19 15:08:22 +0100

    javascript and links

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,6 +4,72 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
+/* EFFET SMOOTH SCOLL */
+
+// Pour tous les liens commençant par #.
+$("a[href^='#']").click(function (e) {
+  var 
+    yPos,
+    yInitPos,
+    target = ($($(this).attr("href") + ":first"));
+    
+  // On annule le comportement initial au cas ou la base soit différente de la page courante.
+  e.preventDefault(); 
+  
+  yInitPos = $(window).scrollTop();
+  
+  // On ajoute le hash dans l'url.
+  window.location.hash = $(this).attr("href");
+  
+  // Comme il est possible que l'ajout du hash perturbe le défilement, on va forcer le scrollTop à son endroit inital.
+  $(window).scrollTop(yInitPos);
+  
+  // On cible manuellement l'ancre pour en extraire sa position.
+  // Si c'est un ID on l'obtient.
+  target = ($($(this).attr("href") + ":first"));
+
+  // Sinon on cherche l'ancre dans le name d'un a.
+  if (target.length == 0) {
+    target = ($("a[name=" + $(this).attr("href").replace(/#/gi,"") + "]:first"))
+  }
+  
+  // Si on a trouvé un name ou un id, on défile.
+  if (target.length == 1) {
+    yPos = target.offset().top; // Position de l'ancre.
+  
+          // La largeur minimale de l'écran correspond
+    if (window.matchMedia ('(min-width: 1000px)').matches)
+    {
+      $('html,body').animate({ scrollTop: yPos - 60 }, 1000);
+    }
+    // Sinon
+    else
+    {
+      $('html,body').animate({ scrollTop: yPos - 60 }, 1000);
+    }
+    // On anime le défilement jusqu'à l'ancre.
+     // On décale de 190 pixels l'affichage pour ne pas coller le bord haut de l'affichage du navigateur et on défile en 1 seconde jusqu'à l'ancre.
+  }
+});
+
+/* ~~~~~~~~~~~~~~~~~~~ */
+
+$(document).ready(function(){
+$(window).bind('scroll', function() {
+var navHeight = $( window ).height() - 81;
+     if ($(window).scrollTop() > navHeight) {
+       $('.up').addClass('fixed');
+       $("#top-anchor").css({bottom: '30px'});
+     }
+     else {
+       $('.up').removeClass('fixed');
+       $("#top-anchor").css({bottom: '-100px'});
+     }
+  });
+});
+
+
+
 $('.slider').each(function() {
   var $this = $(this);
   var $group = $this.find('.slide_group');
@@ -92,4 +158,20 @@ $('.slider').each(function() {
   });
   
   advance();
+});
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* navbar */
+
+$(document).ready(function() {
+        // Transition effect for navbar 
+        $(window).scroll(function() {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+          if($(this).scrollTop() > 100) { 
+              $('.navbar-lewagon').addClass('transparent');
+          } else {
+              $('.navbar-lewagon').removeClass('transparent');
+          }
+        });
 });
