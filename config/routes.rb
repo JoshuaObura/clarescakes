3459d54c724a0837d289dba868698fbf147f9987
jh <oluashegun@googlemail.com>
2020-10-29 11:34:57 +0000

    mailer set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
   get 'home', to:'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  require "sidekiq/web"
+  authenticate :user, ->(user) { user.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
