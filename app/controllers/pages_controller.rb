8a503dcbc44831a8380a8d1f6d2fba5a86479aac
Josh <64604325+JoshuaObura@users.noreply.github.com>
2020-11-07 19:00:20 +0000

    Revert "email form fix"

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :cakes, :contact ]
+  skip_before_action :authenticate_user!, only: [ :home, :cakes, :classes ]
 
   def home
     @contact = Contact.new
   end
+  
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
-  config.action_mailer_default_url_options = { host: 'http://localhost:3000/' }
-  Rails.application.routes.default_url_options[:host] = 'http://localhost:3000/'
+  config.action_mailer_default_url_options = { host: 'https://your-app-site.com' }
+  Rails.application.routes.default_url_options[:host] = 'https://your-app-site.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -12,8 +12,8 @@ Rails.application.configure do
     domain: 'gmail.com'
     authentication: 'plain'
     enable_starttls_auto: true
-    user_name: ENV['joshobura']
-    password: ENV['ftyiiobzciegkrvg']
+    user_name: ENV['GMAIL_USERNAME']
+    password: ENV['GMAIL_PASSWORD']
   }
   # Settings specified here will take precedence over those in config/application.rb.
 
